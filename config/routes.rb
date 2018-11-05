2cf0e1562e3afbf3e7d12a30f6da4bbfa72ae5bb
victorhebdo <victor@gabella.org>
2018-11-05 19:33:36 +0000

    maj user/micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'user#index'
+
+  get '/users', to: 'users#index'
+  get '/miscroposts', to: 'miscroposts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
